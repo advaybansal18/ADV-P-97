a527346e52416bd2f334c8d020c2fb831b188ced
advaybansal18 <81328082+advaybansal18@users.noreply.github.com>
2021-09-13 14:32:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+r=random(255);
+g=random(255);
+b=random(255);
+document.getElementById("title").innerHTML=(r,g,b);
+document.getElementById("author").innerHTML=(r,g,b);
+document.getElementById("passage").innerHTML=(r,g,b);
